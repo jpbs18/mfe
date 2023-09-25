31876746284b5b1e64745445c788cbf958097fd3
João Santos <joao.santos@mindswap.academy>
2023-09-25 21:48:25 +0100

    Refactored MarketingApp

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,11 +2,7 @@ import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
 
 export default () => {
-    const ref = useRef(null);
-
-    useEffect(() => {
-        mount(ref.current);
-    }, []);
-
-    return <div ref={ref}></div>;
+  const ref = useRef(null);
+  useEffect(() => mount(ref.current), []);
+  return <div ref={ref}></div>;
 };
