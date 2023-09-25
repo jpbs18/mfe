8f522b8d340514b476153a70c29401a6c6fd5a86
João Santos <joao.santos@mindswap.academy>
2023-09-25 21:10:59 +0100

    Created container deploy script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,2 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${process.env.PRODUCTION_DOMAIN}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies
+    })
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
